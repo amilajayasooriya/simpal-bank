d5a0f212e00300460a6264fa2257c0fda196afe4
amila <amilajayasooriya@gmail.com>
2021-11-14 01:33:41 +0530

    setting up project structure

---
+++ simple-bank-core/src/test/java/core/SimpleBankCoreTest.java
@@ -0,0 +1,13 @@
+package java.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleBankCoreTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
