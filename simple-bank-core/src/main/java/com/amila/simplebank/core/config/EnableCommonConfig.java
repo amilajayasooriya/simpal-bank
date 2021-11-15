7019c8dc71f52eecf2ca3a69dcadecb1f3ebedc6
amila <amilajayasooriya@gmail.com>
2021-11-15 17:12:09 +0530

    enable swagger UI http://localhost:10444/swagger-ui/

--- simple-bank-core/src/main/java/com/amila/simplebank/core/config/EnableCommonConfig.java
+++ simple-bank-core/src/main/java/com/amila/simplebank/core/config/EnableCommonConfig.java
@@ -1,11 +1,25 @@
 package com.amila.simplebank.core.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @PropertySource(value = {"classpath:simple-bank-core-config.properties"})
+@EnableSwagger2
 public class EnableCommonConfig{
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
